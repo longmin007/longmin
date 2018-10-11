6dffb17e3d153338b2d0609c501871d483b56817
longmin007 <44048971+longmin007@users.noreply.github.com>
2018-10-11 16:23:38 +0800

    Update test.java

--- Git/src/test.java
+++ Git/src/test.java
@@ -3,5 +3,6 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("my names longmi");
 		System.out.println("my names liuman");
+		System.out.println("my names liuman");
 	}
 }
